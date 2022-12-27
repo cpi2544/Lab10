2edc366663ebc514a9d946b581b05a167b6576cd
cpi2544 <pokpong1789@gmail.com>
2022-12-27 15:14:32 +0700

    lab10.cpp completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int a,int b){
+	if(a <= 0 || b  <= 0)
+		cout << "Invalid input";
+	else 
+		for(int i = 0;i < a; i++){
+			for(int j = 0;j < b; j++){
+				cout << "O";
+			}
+			cout << "\n";
+		}
+
+
+
+}
 
 int main(){
 	
